37348e0b113ec0777ad830a744009424149ffe78
paras-lalchandani <paraslalchandani2@gmail.com>
2022-04-11 17:02:49 +0530

    C# SFTP Client

---
+++ c#/SFTP/SFTPService/SftpService.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Extensions.Logging;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+namespace SFTPService
+{
+    /// <summary>
+    /// Class containing sftp file operations
+    /// </summary>
+    public class SftpService : ISftpService
+    {
+        private readonly ILogger<SftpService> _logger;
+        private readonly SftpConfig _config;
+
+        public SftpService(ILogger<SftpService> logger, SftpConfig sftpConfig)
+        {
+            _logger = logger;
+            _config = sftpConfig;
+        }
+
+        /// <summary>
+        /// Get list of all the files in remote directory
+        /// </summary>
+        /// <param name="remoteDirectory">is the directory path.</param>
+        /// <returns>IEnumerable<SftpFile> , containing collection of files in the given directory</returns>
+       
+        public IEnumerable<SftpFile> ListAllFiles(string remoteDirectory = ".")
+        {
+            using var client = new SftpClient(_config.Host, _config.Port == 0 ? 22 : _config.Port, _config.UserName, _config.Password);
+            try
+            {
+                client.Connect();
+                return client.ListDirectory(remoteDirectory);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Failed in listing files under [{remoteDirectory}]");
+                return null;
+            }
+            finally
+            {
+                client.Disconnect();
+            }
+        }
+
+        /// <summary>
+        /// Upload the files from a local directory to the remote directory
+        /// </summary>
+        /// <param name="localFilePath">is the source path of the file .</param>
+        /// <param name="remoteFilePath">is the destination path of the file .</param>
+        /// <returns>void</returns>
+        public void UploadFile(string localFilePath, string remoteFilePath)
+        {           
+            using var client = new SftpClient(_config.Host, _config.Port == 0 ? 22 : _config.Port, _config.UserName, _config.Password);
+            try
+            {
+                client.Connect();
+                using var s = File.OpenRead(localFilePath);
+                client.UploadFile(s, remoteFilePath);
+                _logger.LogInformation($"Finished uploading file [{localFilePath}] to [{remoteFilePath}]");
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Failed in uploading file [{localFilePath}] to [{remoteFilePath}]");
+            }
+            finally
+            {
+                client.Disconnect();
+            }
+        }
+
+        /// <summary>
+        /// download the files from remote location to local path
+        /// </summary>
+        /// <param name="localFilePath">is the source path of the file .</param>
+        /// <param name="remoteFilePath">is the destination path of the file .</param>
+        /// <returns>void</returns>
+        public void DownloadFile(string remoteFilePath, string localFilePath)
+        {
+            using var client = new SftpClient(_config.Host, _config.Port == 0 ? 22 : _config.Port, _config.UserName, _config.Password);
+            try
+            {
+                client.Connect();
+                using var s = File.Create(localFilePath);
+                client.DownloadFile(remoteFilePath, s);
+                _logger.LogInformation($"Finished downloading file [{localFilePath}] from [{remoteFilePath}]");
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Failed in downloading file [{localFilePath}] from [{remoteFilePath}]");
+            }
+            finally
+            {
+                client.Disconnect();
+            }
+        }
+
+        /// <summary>
+        /// delete the remote files 
+        /// </summary>     
+        /// <param name="remoteFilePath">is the path of the file to be deleted .</param>
+        /// <returns>void</returns>
+        public void DeleteFile(string remoteFilePath)
+        {
+            using var client = new SftpClient(_config.Host, _config.Port == 0 ? 22 : _config.Port, _config.UserName, _config.Password);
+            try
+            {
+                client.Connect();
+                client.DeleteFile(remoteFilePath);
+                _logger.LogInformation($"File [{remoteFilePath}] deleted.");
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Failed in deleting file [{remoteFilePath}]");
+            }
+            finally
+            {
+                client.Disconnect();
+            }
+        }
+    }
+}
