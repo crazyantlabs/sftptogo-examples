c384c75c33d9c1a3ea67fe5cabd7fddc735765e2
paras-lalchandani <paraslalchandani2@gmail.com>
2022-04-11 17:02:49 +0530

    C# SFTP Client

---
+++ c#/SFTP/SFTPClient/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Logging.Abstractions;
+using SFTPService;
+
+
+
+// Setup SFTP Configurations below and run the code directly 
+
+    var config = new SftpConfig
+    {
+        Host = "", 
+        Port = 0,
+        UserName = "",
+        Password = "" // 
+    }; 
+
+// Create the sftp service object 
+
+    var sftpService = new SftpService(new NullLogger<SftpService>(), config);
+
+// list files
+
+    var files = sftpService.ListAllFiles("/directory/folder");
+    foreach (var file in files)
+    {
+        if (file.IsDirectory)
+        {
+            Console.WriteLine($"Directory: [{file.FullName}]");
+        }
+        else if (file.IsRegularFile)
+        {
+            Console.WriteLine($"File: [{file.FullName}]");
+        }
+    }
+
+// download a file
+
+const string pngFile = @"test.png";
+File.Delete(pngFile);
+sftpService.DownloadFile(@"/folder/imap-console-client.png", pngFile);
+
+if (File.Exists(pngFile))
+{
+    Console.WriteLine($"file {pngFile} downloaded");
+}
+
+
+// upload a file 
+var testFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+sftpService.UploadFile(testFile, @"/folder/test.txt");
+
+// delete a file
+sftpService.DeleteFile(@"/folder/test.txt");
---
+++ c#/SFTP/SFTPService/ISftpService.cs
@@ -0,0 +1,41 @@
+﻿using Renci.SshNet.Sftp;
+
+namespace SFTPService
+{
+    /// <summary>
+    /// This interface contains methods that are supported by our sftp service 
+    /// </summary>
+    public interface ISftpService
+    {
+        /// <summary>
+        /// Get list of all the files in remote directory
+        /// </summary>
+        /// <param name="remoteDirectory">is the directory path.</param>
+        /// <returns>IEnumerable<SftpFile> , containing collection of files in the given directory</returns>
+        IEnumerable<SftpFile> ListAllFiles(string remoteDirectory = ".");
+
+        /// <summary>
+        /// Upload the files from a local directory to the remote directory
+        /// </summary>
+        /// <param name="localFilePath">is the source path of the file .</param>
+        /// <param name="remoteFilePath">is the destination path of the file .</param>
+        /// <returns>void</returns>
+        void UploadFile(string localFilePath, string remoteFilePath);
+
+        /// <summary>
+        /// download the files from remote location to local path
+        /// </summary>
+        /// <param name="localFilePath">is the source path of the file .</param>
+        /// <param name="remoteFilePath">is the destination path of the file .</param>
+        /// <returns>void</returns>
+        void DownloadFile(string remoteFilePath, string localFilePath);
+
+
+        /// <summary>
+        /// delete the remote files 
+        /// </summary>     
+        /// <param name="remoteFilePath">is the path of the file to be deleted .</param>
+        /// <returns>void</returns>
+        void DeleteFile(string remoteFilePath);
+    }
+}
