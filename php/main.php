15abd5f5d7a8908e2d7fa22e8b0180afc4f4fc3e
saggineumann <1950417+saggineumann@users.noreply.github.com>
2023-06-07 11:27:52 +0300

    Update main.php

--- php/main.php
+++ php/main.php
@@ -1,70 +1,61 @@
 <?php
 
+use phpseclib3\Net\SFTP;
+
 class SFTPClient
 {
-  private $connection;
   private $sftp;
 
   public function __construct($host, $port=22)
   {
-    $this->connection = @ssh2_connect($host, $port);
-    if (! $this->connection)
+    $this->sftp = new SFTP($host, $port);
+    if (!$this->sftp->isConnected())
       throw new Exception("Failed to connect to ${host} on port ${port}.");
   }
 
   // Login with user and password
   public function auth_password($username, $password)
   {
-    if (! @ssh2_auth_password($this->connection, $username, $password))
+    if (!$this->sftp->login($username, $password))
       throw new Exception("Failed to authenticate with username $username " .
                           "and password.");
-
-    $this->sftp = @ssh2_sftp($this->connection);
-    if (! $this->sftp)
-      throw new Exception("Could not initialize SFTP subsystem.");
   }
 
   // Login with SSH agent
   public function auth_agent($username)
   {
-    if (! @ssh2_auth_agent($this->connection, $username))
+    if (!$this->sftp->login($username))
       throw new Exception("Failed to authenticate with username $username " .
                           "and public key.");
-
-    $this->sftp = @ssh2_sftp($this->connection);
-    if (! $this->sftp)
-      throw new Exception("Could not initialize SFTP subsystem.");
   }
 
+  // Disconnect session
+  public function disconnect()
+  {
+    $this->sftp->disconnect();
+  }
+  
   // List remote directory files
   function listFiles($remote_dir) {
-    $sftp = $this->sftp;
-    $realpath = ssh2_sftp_realpath($sftp, $remote_dir);
-    $dir = "ssh2.sftp://$sftp$realpath";
     $tempArray = array();
 
     fwrite(STDOUT, "Listing [${remote_dir}] ...\n\n");
 
-    if (is_dir($dir)) {
-      if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false) {
-          $realpath = ssh2_sftp_realpath($sftp, $file);
-          $filetype = filetype($dir . $realpath);
-          $modTime = "";
-          $size = sprintf("%.2f", filesize($dir . $realpath));
+    $files = $this->sftp->nlist($remote_dir);
+    foreach ($files as $file) {
+      $filetype = $this->sftp->is_dir($file) ? 'dir' : 'file';
+      $modTime = '';
+      $size = sprintf("%.2f", $this->sftp->size($file));
 
-          if($filetype == "dir") {
-            $file = $file . "/";
-            $modTime = "";
-            $size = "PRE";
-          }
-
-          $tempArray[] = $file;
-
-          printf("%19s %12s %s\n", $modTime, $size, $file);
-        }
-        closedir($dh);
+      if ($filetype == "dir") {
+        $file .= '/';
+        $modTime = '';
+        $size = "PRE";
       }
+
+      $tempArray[] = $file;
+
+      printf("%19s %12s %s\n", $modTime, $size, $file);
     }
 
     return $tempArray;
@@ -75,17 +66,12 @@ class SFTPClient
   {
     fwrite(STDOUT, "Uploading [${local_file}] to [${remote_file}] ...\n");
 
-    $sftp = $this->sftp;
-    $realpath = ssh2_sftp_realpath($sftp, $remote_file);
-    $stream = @fopen("ssh2.sftp://$sftp$realpath", 'w');
-    if (! $stream)
-      throw new Exception("Could not open file: $realpath");
-    $data_to_send = @file_get_contents($local_file);
+    $data_to_send = file_get_contents($local_file);
     if ($data_to_send === false)
       throw new Exception("Could not open local file: $local_file.");
-    if (@fwrite($stream, $data_to_send) === false)
+
+    if (!$this->sftp->put($remote_file, $data_to_send))
       throw new Exception("Could not send data from file: $local_file.");
-    @fclose($stream);
   }
 
   // Download remote file to local file
@@ -93,22 +79,17 @@ class SFTPClient
   {
     fwrite(STDOUT, "Downloading [${remote_file}] to [${local_file}] ...\n");
 
-    $sftp = $this->sftp;
-    $realpath = ssh2_sftp_realpath($sftp, $remote_file);
-    $stream = @fopen("ssh2.sftp://$sftp$realpath", 'r');
-    if (! $stream)
-      throw new Exception("Could not open file: $realpath");
-    $contents = fread($stream, filesize("ssh2.sftp://$sftp$realpath"));
-    file_put_contents ($local_file, $contents);
-    @fclose($stream);
+    $contents = $this->sftp->get($remote_file);
+    if ($contents === false)
+      throw new Exception("Could not open remote file: $remote_file.");
+
+    file_put_contents($local_file, $contents);
   }
 
   // Delete remote file
   public function deleteFile($remote_file){
     fwrite(STDOUT, "Deleting [${remote_file}] ...\n");
-    $sftp = $this->sftp;
-    $realpath = ssh2_sftp_realpath($sftp, $remote_file);
-    unlink("ssh2.sftp://$sftp$realpath");
+    $this->sftp->delete($remote_file);
   }
 }
 
@@ -119,21 +100,21 @@ function main()
   // Parse URL
   $parsed_url = parse_url($raw_url);
 
-  if($parsed_url === false)
+  if ($parsed_url === false)
   {
     fwrite(STDERR, "Failed to parse SFTP To Go URL.\n");
     exit(1);
   }
 
   // Get user name and password
-  $user = isset( $parsed_url['user'] ) ? $parsed_url['user'] : null;
-  $pass = isset( $parsed_url['pass'] ) ? $parsed_url['pass'] : null;
+  $user = isset($parsed_url['user']) ? $parsed_url['user'] : null;
+  $pass = isset($parsed_url['pass']) ? $parsed_url['pass'] : null;
 
   // Parse Host and Port
-  $host = isset( $parsed_url['host'] ) ? $parsed_url['host'] : null;
+  $host = isset($parsed_url['host']) ? $parsed_url['host'] : null;
 
   // Port is always 22
-  $port = isset( $parsed_url['port'] ) ? $parsed_url['port'] : 22;
+  $port = isset($parsed_url['port']) ? $parsed_url['port'] : 22;
 
   fwrite(STDOUT, "Connecting to [${host}] ...\n");
 
